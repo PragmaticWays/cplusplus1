4311cf4971c2c2849febf5365c749b84f58f963d
Adam Allard <adam.allard@live.com>
2017-04-16 12:46:14 -0700

    Update quarterly-sales.cpp

--- quarterly-sales.cpp
+++ quarterly-sales.cpp
@@ -1,11 +1,11 @@
 /*
-–---------------------------------------------------------------------------------------------------------------------------------------- -
+â€“---------------------------------------------------------------------------------------------------------------------------------------- -
 Adam Allard
 CISS - 241
 Week 7
 Assignment 2
 Quarterly Sales
-–---------------------------------------------------------------------------------------------------------------------------------------- -
+â€“---------------------------------------------------------------------------------------------------------------------------------------- -
 Write a program that calculates information about sales during a year. 
 This program will have anarray that will hold the sales totals for 4 divisions 
 (North, South, East and West, stored in an array as strings for the output) and for 4 quarters. 
@@ -34,7 +34,7 @@ South
 East
 West
 Quarter Total
-–---------------------------------------------------------------------------------------------------------------------------------------- -
+â€“---------------------------------------------------------------------------------------------------------------------------------------- -
 */
 
 #include <iostream>
@@ -70,23 +70,17 @@ int main()
 
 	// get the sales info
 	cout << "Enter the sales info:" << endl << endl;
-	for (int strDivIndex = 0; strDivIndex < COLS; strDivIndex++)
-	{
-		if (strDivIndex == (COLS - 1))
-		{
-			for (int i = 0; i < COLS; i++)
-			{
+	for (int strDivIndex = 0; strDivIndex < COLS; strDivIndex++) {
+		if (strDivIndex == (COLS - 1)) {
+			for (int i = 0; i < COLS; i++) {
 				rowTotal = getRowTotal(sales, i);
 				sales[i][strDivIndex] = rowTotal;
 			}
 			break;
 		}
-		for (int strQtrIndex = 0; strQtrIndex < ROWS; strQtrIndex++)
-		{
-			if (strQtrIndex == (ROWS - 1))
-			{
-				for (int i = 0; i < COLS; i++)
-				{
+		for (int strQtrIndex = 0; strQtrIndex < ROWS; strQtrIndex++) {
+			if (strQtrIndex == (ROWS - 1)) {
+				for (int i = 0; i < COLS; i++) {
 					columnTotal = getColumnTotal(sales, i);
 					sales[strQtrIndex][i] = columnTotal;
 				}
@@ -102,38 +96,26 @@ int main()
 
 	// display table
 	cout << "\t\t";
-	for (int i = 0; i < divNum; i++)
-	{
-		if (i < (divNum - 1))
-		{
+	for (int i = 0; i < divNum; i++) {
+		if (i < (divNum - 1)) {
 			cout << setw(12) << qtr[i];
-		}
-		else
-		{
+		} else {
 			cout << setw(15) << qtr[i];
 		}
 
 	}
 	cout << endl;
-	for (int row = 0; row < ROWS; row++)
-	{
+	for (int row = 0; row < ROWS; row++) {
 		cout << div[row];
-		if (row < (ROWS - 1))
-		{
+		if (row < (ROWS - 1)) {
 			cout << "\t\t";
-		}
-		else
-		{
+		} else {
 			cout << "\t";
 		}
-		for (int col = 0; col < COLS; col++)
-		{
-			if (col < (COLS - 1))
-			{
+		for (int col = 0; col < COLS; col++) {
+			if (col < (COLS - 1)) {
 				cout << setw(12) << sales[row][col];
-			}
-			else
-			{
+			} else {
 				cout << setw(15) << sales[row][col];
 			}
 		}
@@ -165,26 +147,20 @@ int main()
 
 // functions
 
-double getTotal(double numbers[][COLS])
-{
-	for (int x = 0; x < (ROWS - 1); x++)
-	{
-		for (int y = 0; y < (COLS - 1); y++)
-		{
+double getTotal(double numbers[][COLS]) {
+	for (int x = 0; x < (ROWS - 1); x++) {
+		for (int y = 0; y < (COLS - 1); y++) {
 			total += numbers[x][y];
 		}
 	}
 	return total;
 }
 
-double getAverage(double numbers[][COLS])
-{
+double getAverage(double numbers[][COLS]) {
 	double index = 0;
 	double avgTot = 0;
-	for (int x = 0; x < (ROWS - 1); x++)
-	{
-		for (int y = 0; y < (COLS - 1); y++)
-		{
+	for (int x = 0; x < (ROWS - 1); x++) {
+		for (int y = 0; y < (COLS - 1); y++) {
 			avgTot += numbers[x][y];
 			index++;
 		}
@@ -193,35 +169,27 @@ double getAverage(double numbers[][COLS])
 	return average;
 }
 
-double getRowTotal(double numbers[][COLS], int row)
-{
+double getRowTotal(double numbers[][COLS], int row) {
 	double rowTot = 0;
-	for (int col = 0; col < (COLS - 1); col++)
-	{
+	for (int col = 0; col < (COLS - 1); col++) {
 		rowTot += numbers[row][col];
 	}
 	return rowTot;
 }
 
-double getColumnTotal(double numbers[][COLS], int col)
-{
+double getColumnTotal(double numbers[][COLS], int col) {
 	double colTot = 0;
-	for (int row = 0; row < (ROWS - 1); row++)
-	{
+	for (int row = 0; row < (ROWS - 1); row++) {
 		colTot += numbers[row][col];
 	}
 	return colTot;
 }
 
-double getHighest(double numbers[][COLS], int &row, int &col)
-{
+double getHighest(double numbers[][COLS], int &row, int &col) {
 	double high = 0;
-	for (int x = 0; x < (ROWS - 1); x++)
-	{
-		for (int y = 0; y < (COLS - 1); y++)
-		{
-			if ((numbers[x][y]) > high)
-			{
+	for (int x = 0; x < (ROWS - 1); x++) {
+		for (int y = 0; y < (COLS - 1); y++) {
+			if ((numbers[x][y]) > high) {
 				row = x;
 				col = y;
 				high = numbers[row][col];
@@ -231,15 +199,11 @@ double getHighest(double numbers[][COLS], int &row, int &col)
 	return high;
 }
 
-double getLowest(double numbers[][COLS], int &row, int &col)
-{
+double getLowest(double numbers[][COLS], int &row, int &col) {
 	double low = numbers[0][0];
-	for (int x = 0; x < (ROWS - 1); x++)
-	{
-		for (int y = 0; y < (COLS - 1); y++)
-		{
-			if ((numbers[x][y]) <= low)
-			{
+	for (int x = 0; x < (ROWS - 1); x++) {
+		for (int y = 0; y < (COLS - 1); y++) {
+			if ((numbers[x][y]) <= low) {
 				row = x;
 				col = y;
 				low = numbers[row][col];
